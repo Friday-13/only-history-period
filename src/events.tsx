ea94e847f016aabeb7bcefe8e5b7b47b9e84dad4
Dmitrii Samsonenko <samsondm97@gmail.com>
2026-01-24 15:22:18 +0300

    feat: add swiper

--- src/events.tsx
+++ src/events.tsx
@@ -1,3 +1,6 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 import styles from "./events.module.scss";
 interface IEvent {
   year: number;
@@ -6,11 +9,30 @@ interface IEvent {
 
 export const Events = ({ events }: { events: IEvent[] }) => {
   return (
-    <div>
-      <div className={styles.event}>
-        <h4>{events[0].year}</h4>
-        <div className={styles.data}>{events[0].data}</div>
-      </div>
-    </div>
+    <Swiper
+      spaceBetween={25}
+      slidesPerView={2}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <div className={styles.event}>
+          <h4>{events[0].year}</h4>
+          <div className={styles.data}>{events[0].data}</div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.event}>
+          <h4>{events[1].year}</h4>
+          <div className={styles.data}>{events[1].data}</div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.event}>
+          <h4>{events[0].year}</h4>
+          <div className={styles.data}>{events[0].data}</div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 };
