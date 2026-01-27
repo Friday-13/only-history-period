c717484f67dea75954d0dda1c06a56116f7e792a
Dmitrii Samsonenko <samsondm97@gmail.com>
2026-01-28 01:42:57 +0300

    feat: implement year animation

--- src/year-period.tsx
+++ src/year-period.tsx
@@ -11,17 +11,36 @@ import { useGSAP } from "@gsap/react";
 export const YearPeriod = ({ start, end }: IYearPeriod) => {
   const startYearRef = useRef<HTMLHeadingElement | null>(null);
   const endYearRef = useRef<HTMLHeadingElement | null>(null);
+
+  const startValueRef = useRef({ value: start });
+  const endValueRef = useRef({ value: end });
+
   useGSAP(() => {
     if (!startYearRef.current) return;
     if (!endYearRef.current) return;
-    gsap.from(startYearRef.current, {
-      textContent: 0,
+
+    gsap.to(startValueRef.current, {
+      value: start,
       duration: 1,
-      ease: "power1.in",
-      snap: { textContent: 1 },
-      stagger: 1,
+      ease: "power1.out",
+      onUpdate() {
+        startYearRef.current!.textContent = Math.round(
+          startValueRef.current.value,
+        ).toString();
+      },
     });
-  });
+
+    gsap.to(endValueRef.current, {
+      value: end,
+      duration: 1,
+      ease: "power1.out",
+      onUpdate() {
+        endYearRef.current!.textContent = Math.round(
+          endValueRef.current.value,
+        ).toString();
+      },
+    });
+  }, [start, end]);
 
   return (
     <div className={styles.year}>
